1063f6ec144c6ff2286da6f03a74b1295a5d477f
Mateusz Miekicki <miekickimateusz@gmail.com>
2021-10-03 21:17:51 +0200

    The necessary modules have been added for the start. Pytest and PyQt.
    A small example can be found in maine.py.

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from os import environ
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+
+def suppress_qt_warnings():
+    environ["QT_DEVICE_PIXEL_RATIO"] = "0"
+    environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+    environ["QT_SCREEN_SCALE_FACTORS"] = "1"
+    environ["QT_SCALE_FACTOR"] = "1"
+
+if __name__ == "__main__":
+    suppress_qt_warnings()
+    app = QApplication([])
+    window = QWidget()
+    layout = QVBoxLayout()
+    layout.addWidget(QPushButton('Top'))
+    layout.addWidget(QPushButton('Bottom'))
+    window.setLayout(layout)
+    window.show()
+    app.exec()
