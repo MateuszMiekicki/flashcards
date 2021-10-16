9dad535c938c97510da295129d31ed2f02a1ed19
Mateusz Miekicki <miekickimateusz@gmail.com>
2021-10-16 23:07:08 +0200

    Base scheme updated in line with today's discussions(16.10.2021)
    Added ability to create subcategories of subcategories. Added settings for the account. Changed the name of the table from "fiches" to "cards". 
    Of the additional improved formatting.

--- resources/db.changelog/init.sql
+++ resources/db.changelog/init.sql
@@ -1,132 +1,91 @@
-CREATE TABLE "role"
-(
-    id            INT,
-
-    role          VARCHAR NOT NULL UNIQUE,
-
+CREATE TABLE "role" (
+    id INT,
+    role VARCHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
-
 INSERT INTO "role"
 VALUES (1, 'admin'),
-       (2, 'moderator'),
-       (3, 'user');
-
-CREATE TABLE user_type
-(
-    id            INT,
-
-    type          VARCHAR NOT NULL UNIQUE,
-
+    (2, 'moderator'),
+    (3, 'user');
+CREATE TABLE user_type (
+    id INT,
+    type VARCHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
-
 INSERT INTO user_type
 VALUES (1, 'standard'),
-       (2, 'premium');
-
-CREATE TABLE "user"
-(
-    id            INT,
-    role_id       INT     NOT NULL,
-    user_type_id  INT     NOT NULL,
-
-    email         VARCHAR NOT NULL UNIQUE,
-    password      VARCHAR NOT NULL,
-
+    (2, 'premium');
+CREATE TABLE "user" (
+    id INT,
+    role_id INT NOT NULL,
+    user_type_id INT NOT NULL,
+    email VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT role_fk
-        FOREIGN KEY (role_id)
-            REFERENCES "role" (id),
-    CONSTRAINT user_type_fk
-        FOREIGN KEY (user_type_id)
-            REFERENCES user_type (id)
+    CONSTRAINT role_fk FOREIGN KEY (role_id) REFERENCES "role" (id),
+    CONSTRAINT user_type_fk FOREIGN KEY (user_type_id) REFERENCES user_type (id)
 );
-
-CREATE TABLE category
-(
-    id            INT,
-    name          VARCHAR NOT NULL UNIQUE,
-
+CREATE TABLE setting (
+    id INT,
+    name VARCHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
-
-CREATE TABLE subcategory
-(
-    id            INT,
-    category_id   INT     NOT NULL,
-
-    name          VARCHAR NOT NULL,
-
+CREATE TABLE user_setting (
+    id INT,
+    user_id INT NOT NULL,
+    setting_id INT NOT NULL,
+    value VARCHAR NOT NULL UNIQUE,
     PRIMARY KEY (id),
-    CONSTRAINT section_fk
-        FOREIGN KEY (category_id)
-            REFERENCES category (id)
+    CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES "user" (id),
+    CONSTRAINT setting_fk FOREIGN KEY (setting_id) REFERENCES setting (id)
 );
-
-CREATE TABLE game_mode
-(
-    id            INT,
-
-    mode          VARCHAR NOT NULL,
-
+CREATE TABLE category (
+    id INT,
+    name VARCHAR NOT NULL UNIQUE,
     PRIMARY KEY (id)
 );
-
-CREATE TABLE set_fiches
-(
-    id            INT,
-    game_mode_id  INT      NOT NULL,
-    category_id   INT      NOT NULL,
-    user_id       INT,
-
-    name         VARCHAR  NOT NULL,
-
+CREATE TABLE subcategory (
+    id INT,
+    category_id INT NOT NULL,
+    subcategory_id INT,
+    name VARCHAR NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT game_mode_fk
-        FOREIGN KEY (game_mode_id)
-            REFERENCES game_mode (id),
-    CONSTRAINT category_fk
-        FOREIGN KEY (category_id)
-            REFERENCES category (id),
-    CONSTRAINT user_fk
-        FOREIGN KEY (user_id)
-            REFERENCES "user" (id)
+    CONSTRAINT section_fk FOREIGN KEY (category_id) REFERENCES category (id)
 );
-
-CREATE TABLE card
-(
-    id            INT,
-    set_fiches_id INT      NOT NULL,
-
-    PRIMARY KEY (id),
-    CONSTRAINT set_fiches_fk
-        FOREIGN KEY (set_fiches_id)
-            REFERENCES set_fiches (id)
+CREATE TABLE game_mode (
+    id INT,
+    mode VARCHAR NOT NULL,
+    PRIMARY KEY (id)
 );
-
-CREATE TABLE issue
-(
-    id            INT,
-    card_id       INT      NOT NULL,
-
-    content       VARCHAR  NOT NULL,
-
+CREATE TABLE set_cards (
+    id INT,
+    game_mode_id INT NOT NULL,
+    subcategory_id INT NOT NULL,
+    user_id INT NOT NULL,
+    name VARCHAR NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT card_fk
-        FOREIGN KEY (card_id)
-            REFERENCES card (id)
+    CONSTRAINT game_mode_fk FOREIGN KEY (game_mode_id) REFERENCES game_mode (id),
+    CONSTRAINT category_fk FOREIGN KEY (subcategory_id) REFERENCES subcategory (id),
+    CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
-
-CREATE TABLE issue_answer
-(
-    id            INT,
-    issue_id      INT      NOT NULL,
-
-    content       VARCHAR  NOT NULL,
-
+CREATE TABLE card (
+    id INT,
+    set_cards_id INT NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT issue_fk
-        FOREIGN KEY (issue_id)
-            REFERENCES issue (id)
+    CONSTRAINT set_cards_fk FOREIGN KEY (set_cards_id) REFERENCES set_cards (id)
+);
+CREATE TABLE question (
+    id INT,
+    card_id INT NOT NULL,
+    content VARCHAR NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT card_fk FOREIGN KEY (card_id) REFERENCES card (id)
+);
+CREATE TABLE question_answer (
+    id INT,
+    question_id INT NOT NULL,
+    content VARCHAR NOT NULL,
+    is_correct_answer BOOLEAN NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT question_fk FOREIGN KEY (question_id) REFERENCES question (id)
 );
